4113162aa9d1add53c0d8ba2dce0f2dddfb91f74
zdzdz <zdzdz.link@yahoo.com>
2016-02-19 02:37:16 +0200

    Added animation for the sidedrawer

--- app/views/home/home-view-model.js
+++ app/views/home/home-view-model.js
@@ -1,13 +1,36 @@
-var observable = require("data/observable");
-var HomeViewModel = (function (_super) {
+'use strict';
+
+let observable = require("data/observable");
+let frameModule = require("ui/frame");
+let drawerModule = require("nativescript-telerik-ui/sidedrawer");
+
+let HomeViewModel = (function (_super) {
     __extends(HomeViewModel, _super);
     function HomeViewModel() {
         _super.call(this);
-        this.set("mainContentText", "SideDrawer for NativeScript can be easily setup in the XML definition of your page by defining main- and drawer-content. The component has a default transition and position and also exposes notifications related to changes in its state.");
-    }
+        this.set("mainContentText", "Test");
+        }
+    
+    HomeViewModel.prototype.setDrawerTransition = function (transition) {
+        let drawer = frameModule.topmost().getViewById("sideDrawer");
+        drawer.closeDrawer();
+        drawer.drawerTransition = transition;
+    };
+
+    HomeViewModel.prototype.onScaleDownPusherTransitionTap = function (args) {
+        let drawer = frameModule.topmost().getViewById("sideDrawer");
+        this.setDrawerTransition(new drawerModule.ScaleDownPusherTransition());
+        drawer.showDrawer();
+    };
     
     return HomeViewModel;
 })(observable.Observable);
+
+// let onPushTransitionTap = function () {
+//         let drawer = frameModule.topmost().getViewById("sideDrawer");
+//         drawer.drawerTransition = new drawerModule.ScaleDownPusherTransition();
+//         drawer.showDrawer();
+//     };
 
 exports.HomeViewModel = HomeViewModel;
 exports.homeViewModel = new HomeViewModel();
