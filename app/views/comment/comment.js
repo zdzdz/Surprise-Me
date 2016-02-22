f65d05173a21a7d2216270544e2d170e88132cbb
YordanGergov <yordan.gergov91@gmail.com>
2016-02-22 19:48:57 +0200

    work in progress

--- app/views/comment/comment.js
+++ app/views/comment/comment.js
@@ -1,10 +1,31 @@
 'use strict';
 
 let vmModule = require("./comment-view-model");
+let textFieldModule = require("ui/text-field");
+let observableAuthor = require("data/observable");
+let observableComment = require("data/observable");
 
 function pageLoaded(args) {
-    let page = args.object;
-    page.bindingContext = vmModule.commentViewModel;
+  var page = args.object;
+  var objAuthor = new observableAuthor.Observable();
+  var objComment = new observableComment.Observable();
+  objComment.set("Comment", "Enter your comment here.");
+  objAuthor.set("Author", "Enter your name here.");
+
+  var bindingOptions = {
+    sourceProperty: "textSource",
+    targetProperty: "text",
+    twoWay: true
+};
+
+  page.bindingContext = objAuthor;
+  page.bindingContext = objComment; 
+}
+
+function sendComment(args){
+
 }
 
 exports.pageLoaded = pageLoaded;
+exports.sendComment = sendComment;
+
